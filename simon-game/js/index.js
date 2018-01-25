6777cd935a58aa85640e7d9fb98ecbc49ee98b38
Matt Young <mattgyoung99@gmail.com>
2018-01-24 23:55:28 -0600

    switch statement

--- simon-game/js/index.js
+++ simon-game/js/index.js
@@ -1,43 +1,24 @@
-var round = 0, color, sequence=[], x=0;
+var round = 0, sequence=[], x=0;
 
 function addRound() {
-   color = Math.round(Math.random()*3);
-   if (color === 0) {
+   switch (Math.round(Math.random()*3)) {
+    case 0:
     sequence.push('one');
-  } else if (color === 1) {
+    case 1:
     sequence.push('two');
-  } else if (color === 2) {
+    case 2:
     sequence.push('three');
-  } else if (color === 3) {
+    case 3:
     sequence.push('four');
- }
-  console.log(sequence);
+  }
 }
+
 function reset() {
-  turn = 0;
   sequence = [];
   round = sequence.length;
   document.getElementById('c').value = sequence.length;
 }
-function game(arr) {
-  var elem = document.querySelectorAll('.gameButton');
-  if (arr.length === 0) {
-    beginRound();
-  } else {
-    for (var k=0;k<elem.length;k++) {
-      elem[k].addEventListener("click", function() {
-        if (this.id == arr[0]) {
-          console.log('right!');
-          console.log(arr);
-          game(arr);
-        } else {
-          console.log('oops, wrong button!');
-          reset();
-        }
-      })
-    }
-  }
-}
+
 function loopIt() {
   if (x<=round && sequence.length<21) {
     $('#one, #two, #three, #four, #strt').prop('disabled',true);
@@ -54,10 +35,18 @@ function loopIt() {
     $('#one, #two, #three, #four, #strt').prop('disabled',false);
   }
 }
+
+
+function game() {
+  var elem = document.querySelectorAll('.gameButton');
+
+}
+
+
 function beginRound() {
   addRound();
   loopIt();
-  game(sequence);
+  game();
 }
 
 
