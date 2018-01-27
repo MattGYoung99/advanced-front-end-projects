3d643e6a84019ba26138cffa5f893d6aa5088ecd
Matt Young <mattgyoung99@gmail.com>
2018-01-26 21:31:33 -0600

    simon

--- simon-game/js/index.js
+++ simon-game/js/index.js
@@ -1,24 +1,9 @@
 var round = 0, sequence=[], x=0;
-
-function addRound() {
-   switch (Math.round(Math.random()*3)) {
-    case 0:
-    sequence.push('one');
-    case 1:
-    sequence.push('two');
-    case 2:
-    sequence.push('three');
-    case 3:
-    sequence.push('four');
-  }
-}
-
 function reset() {
   sequence = [];
   round = sequence.length;
   document.getElementById('c').value = sequence.length;
 }
-
 function loopIt() {
   if (x<=round && sequence.length<21) {
     $('#one, #two, #three, #four, #strt').prop('disabled',true);
@@ -35,18 +20,30 @@ function loopIt() {
     $('#one, #two, #three, #four, #strt').prop('disabled',false);
   }
 }
-
-
-function game() {
-  var elem = document.querySelectorAll('.gameButton');
-
+function addRound() {
+  switch (Math.round(Math.random()*3)) {
+   case 0:
+   sequence.push('one');
+   break;
+   case 1:
+   sequence.push('two');
+   break;
+   case 2:
+   sequence.push('three');
+   break;
+   case 3:
+   sequence.push('four');
+   break;
+ }
+ loopIt();
 }
-
-
+function game($element) {
+  var elem = document.querySelectorAll('.gameButton');
+  console.log(sequence);
+  console.log($element.id);
+}
 function beginRound() {
   addRound();
-  loopIt();
-  game();
 }
 
 
