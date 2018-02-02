1b69847937ac4eb696a50fe3556842e94e2dac5d
Matt Young <mattgyoung99@gmail.com>
2018-02-01 23:53:37 -0600

    Simon Game JS

--- simon-game/js/index.js
+++ simon-game/js/index.js
@@ -1,14 +1,7 @@
-$(document).ready(function() {
   var round = 0, x=0, currSequence=[], sequence = [];
   // console.log(sequence + '*', currSequence)
   var checkToAddRound = function(arr) {
     return (arr.length === 0 ? (true) : (false)); 
-  }
-  function reset() {
-    sequence = [];
-    currSequence = [];
-    round = sequence.length;
-    document.getElementById('c').value = sequence.length;
   }
   function loopIt() {
     if (x<=round && sequence.length<21) {
@@ -21,28 +14,47 @@ $(document).ready(function() {
       }, 1000)
     } else {
       x=0;
-      round++;
-      document.getElementById('c').value = round;
+      round = sequence.length;
+      document.getElementById('c').value = sequence.length;
       $('#one, #two, #three, #four, #strt, #reset').prop('disabled',false);
     }
   }
   function addRound() {
-    switch (Math.round(Math.random()*3)) {
-    case 0:
-    sequence.push('one');
-    break;
-    case 1:
-    sequence.push('two');
-    break;
-    case 2:
-    sequence.push('three');
-    break;
-    case 3:
-    sequence.push('four');
-    break;
+      switch (Math.round(Math.random()*3)) {
+      case 0:
+      sequence.push('one');
+      break;
+      case 1:
+      sequence.push('two');
+      break;
+      case 2:
+      sequence.push('three');
+      break;
+      case 3:
+      sequence.push('four');
+      break;
+    }
+    loopIt();
+    for (var x in sequence) {
+      currSequence.push(sequence[x])
+    }
   }
-  loopIt();
-  for (var x in sequence) {currSequence.push(sequence[x])}
+  function reset(val) {
+    if (val === true) {
+      sequence = [];
+      currSequence = [];
+      round = sequence.length;
+      document.getElementById('c').value = sequence.length;
+    } else {
+      sequence.unshift();
+      currSequence = []
+      round = sequence.length;
+      document.getElementById('c').value = sequence.length;
+      loopIt();
+      for (var x in sequence) {
+        currSequence.push(sequence[x])
+      }
+    }
   }
   function game($element) {
     if (checkToAddRound(currSequence)) {
@@ -50,16 +62,24 @@ $(document).ready(function() {
     } else {
       if ($element.id === currSequence[0]) {
         currSequence.shift()
-        if (currSequence.length === 0) {addRound()} 
+        if (currSequence.length === 0) {
+          addRound()
+        } 
         console.log('Right!');
       } else {
         console.log('Wrong!');
-        reset();
+        if ($('#strictmode').prop('checked')) {
+          console.log('restarting game');
+          reset(true)
+        } else {
+          console.log('restarting round');
+          reset(false);
+        }
       }
     }
   }
   $(function() {
-    $("#celciusfahrenheit").change(function() {
+    $("#strictmode").change(function() {
       if ($(this).prop('checked')) {
         console.log('checked');
       } else {
@@ -67,4 +87,3 @@ $(document).ready(function() {
       }
     }) 
   }); 
-});
